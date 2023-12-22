e6b619bd73602e804ec6858ce5f7ba9e75234058
Gidon Abbas <89697081+Jadaan59@users.noreply.github.com>
2023-12-22 14:59:22 +0200

    Update GenThree.java

--- GenThree.java
+++ GenThree.java
@@ -4,6 +4,17 @@
  */
 public class GenThree {
 	public static void main(String[] args) {
-		// Put your code here	
+		// First, give the variables the command line arguments.
+
+        int min = Integer.parseInt(args[0]);
+        int max = Integer.parseInt(args[1]);
+		
+			// print out three generated random number in the range [min, max).	
+
+        System.out.println((int)(Math.random() * (max - min) + min));
+        System.out.println((int)(Math.random() * (max - min) + min));
+        System.out.println((int)(Math.random() * (max - min) + min));
+        System.out.println("The minimal generated number was " + "" + min);
+ 	
 	}
 }
