7a85d6a417dee01f1f8e86c6dde4816d4f2f8cc4
Gidon Abbas <89697081+Jadaan59@users.noreply.github.com>
2023-12-22 15:02:19 +0200

    Update LinearEq.java

--- LinearEq.java
+++ LinearEq.java
@@ -5,5 +5,18 @@
  * Treats the three arguments as well as the computed value as double values
  */
 public class LinearEq {
-	// Put your code here
+    public static void main(String[] args) { 
+
+		double a = Double.parseDouble(args[0]);
+		double b = Double.parseDouble(args[1]);
+        	double c = Double.parseDouble(args[2]);
+
+        		// We need the variables to be doubles, 
+        		// because the devision is not always a natural number.
+
+    	System.out.println(a + " * x + " + b + " = "  + c);                     
+	System.out.println("x = " + ((c-b)/a)); 
+				
+	// algorithem to solve the linear equation.
+    }
 }
