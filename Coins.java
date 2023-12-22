04bc46a8ea2448dc6f600689f3eea046f600b2c7
Gidon Abbas <89697081+Jadaan59@users.noreply.github.com>
2023-12-22 15:01:11 +0200

    Update Coins.java

--- Coins.java
+++ Coins.java
@@ -4,6 +4,13 @@
  */
 public class Coins {
 	public static void main(String[] args) {
-		// Put your code here
+		int a = Integer.parseInt(args[0]);
+		int quarter = 25;
+		
+	System.out.println("use " + (a / quarter) + " quarters and " + (a % quarter) + " cents ");
+ 
+                         // Because of being "a" and "quarters" an integers,
+			// so the devision between them is an integer number.
+ 
 	}
 }
