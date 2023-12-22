69784ea838c09ca12e02b19741198c7fa908478d
Gidon Abbas <89697081+Jadaan59@users.noreply.github.com>
2023-12-22 15:03:14 +0200

    Update Triangle.java

--- Triangle.java
+++ Triangle.java
@@ -5,6 +5,17 @@
  */ 
 public class Triangle {
 	public static void main(String[] args) {
-		// Put your code here	
+		
+	int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        int c = Integer.parseInt(args[2]);
+        boolean length; // What is the defenetion of length?
+
+        length = (a+b>c && a+c>b && b+c>a && b+a>c && c+b>a && c+a>b);
+
+                // The all combenations of the sides of the triangle. 
+
+    System.out.println( a + ", " + b + ", " + c + ": " + length);
+ 	
 	}
 }
